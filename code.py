f560078539f8cb32fdb7149c68f2c8ed95a87b3f
Daniel James <daniel@thzinc.com>
2024-06-26 16:00:21 -0700

    (+semver: fix) Increase PWM frequency to 22kHx and use hex for 16-bit ints

--- code.py
+++ code.py
@@ -5,14 +5,14 @@ import digitalio
 import pwmio
 
 FAN_SPEEDS = [
-    {"pixel": (255, 0, 0), "duty_cycle": 0},  # red, 0%
-    {"pixel": (255, 255, 0), "duty_cycle": 21843},  # yellow, 33%
-    {"pixel": (0, 255, 0), "duty_cycle": 43690},  # green, 67%
-    {"pixel": (0, 0, 255), "duty_cycle": 65535},  # blue, 100%
+    {"pixel": (255, 0, 0), "duty_cycle": 0x0000},  # red, 0%
+    {"pixel": (255, 255, 0), "duty_cycle": 0x5555},  # yellow, 33%
+    {"pixel": (0, 255, 0), "duty_cycle": 0xAAAA},  # green, 67%
+    {"pixel": (0, 0, 255), "duty_cycle": 0xFFFF},  # blue, 100%
 ]
 
 pixel = neopixel.NeoPixel(board.NEOPIXEL, 1)
-fan = pwmio.PWMOut(board.A2, frequency=50)
+fan = pwmio.PWMOut(board.A2, frequency=22000)
 button = digitalio.DigitalInOut(board.A0)
 button.direction = digitalio.Direction.INPUT
 button.pull = digitalio.Pull.UP
@@ -25,8 +25,8 @@ def set_state(selected_fan_speed):
     print(f"Setting pixel to {pixel_fill}")
     pixel.fill(pixel_fill)
 
-    print(f"Setting fan duty cycle to {fan_duty_cycle}")
     fan.duty_cycle = fan_duty_cycle
+    print(f"Set fan duty cycle to {fan.duty_cycle}")
 
 
 button_up = True
